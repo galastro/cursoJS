1808a3e9014ee68391726d7137e636f691afcb35
Leandro Galastro Caiafa <95431162+galastro@users.noreply.github.com>
2022-01-29 20:33:32 -0300

    Array intro

---
+++ aula015/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 2, 9, 3]
+num.sort()
+num.push(1)
+console.log(`Este array possui ${num.length} elementos.`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(num)
---
+++ aula015/vetornatela.js
@@ -0,0 +1,28 @@
+let valores = [8, 1, 7,  2, 9]
+
+for(let pos in valores) {
+    console.log(valores[pos])
+}
+
+/*
+for(c = 0; c < valores.length; c++) {
+    console.log(`chave ${c}: ${valores[c]}`)
+
+    
+}
+
+
+
+ 
+for(c = 0; c < valores.length; c++) {
+    console.log(valores[c])
+}
+
+
+let c = 0
+while (c<valores.length) {
+    console.log(valores[c])
+    c++
+
+}
+*/
