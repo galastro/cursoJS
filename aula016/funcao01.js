3233b4f52c3185daed5f27009e3512ddfc6cab6e
Leandro Galastro Caiafa <95431162+galastro@users.noreply.github.com>
2022-02-03 15:40:41 -0300

    Ultima aula

---
+++ aula016/funcao01.js
@@ -0,0 +1,12 @@
+function parimpar(n) {
+    if (n%2==0) {
+        return 'par'
+    } else {
+        return 'ímpar'
+    }
+}
+console.log(parimpar(3))
+/*
+let res = parimpar(3)
+console.log(`${res}`)
+*/
